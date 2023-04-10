62dfea73dbbf070dab3e676fc32a33b77021cc6b
Enatielly Goes <enatielly.rosane@gmail.com>
2023-04-10 11:26:07 -0300

    Session about print commands and variables.

---
+++ session_1/print.py
@@ -0,0 +1,15 @@
+"""print('Hello World')
+
+print(" ") # for space between prints
+
+print("Hello World\nHello World") # \n for print in 2 lines the that do you want print
+
+print(" ")
+
+print("Hello"+" "+"Nat") # + to concatenate strings on print
+"""
+# print("Hello " + input("What is your name?")+ "!")
+
+print( len( input("What is your name? ") ) )
+
+
---
+++ session_1/variables.py
@@ -0,0 +1,8 @@
+# name = input("What is your name? ")
+# print(name)
+
+# print( len( input("What is your name? ") ) )
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
