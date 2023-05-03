230ca4cb5d24951bb76cdf9fe22397a492e96c98
Enatielly Goes <enatielly.rosane@gmail.com>
2023-05-03 19:06:26 -0300

    Exercise of Day 5 - For loop -  of 100 days of Python by Angela Yu.

---
+++ session_5/exercise_loop_average_height.py
@@ -0,0 +1,20 @@
+# Don't change the code below:
+
+student_heights = input("Input a list of student heights ").split()
+
+for n in range (0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+print(student_heights)
+
+#Write your code below this row
+total_height = 0
+for height in student_heights:
+    total_height += height
+print(total_height)
+
+number_of_students = 0
+for student in student_heights:
+    number_of_students += 1
+print(f"There are a total of {number_of_students} heights in student heights.")
+print(f"The average height is: {round(total_height / number_of_students)}")
+
